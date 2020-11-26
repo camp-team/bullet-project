63a8cc91e0cf564e1bf42b45174470e275976d6f
Shiori <hontocha.prog@gmail.com>
2020-11-26 20:28:13 +0900

    feat(deploy): firebase deploy実行（途中）

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,15 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const config = {
+  buildDir: 'nuxt',
+  dev: false,
+  debug: true,
+}
+
+const nuxt = new Nuxt(config)
+
+exports.ssr = functions.https.onRequest(async (request, response) => {
+  await nuxt.ready()
+  return nuxt.render(request, response)
+})
