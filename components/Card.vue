ed3d8f4f0a8cc6aa7fdfb5eeb7fd6619dec685af
Shiori <hontocha.prog@gmail.com>
2020-12-24 23:55:41 +0900

    feat(top): 報告機能追加 #21

--- components/Card.vue
+++ components/Card.vue
@@ -15,18 +15,67 @@
           }}</v-list-item-title>
         </v-list-item-content>
         <v-row
-          v-if="
-            this.$route.name === 'index' &&
-            user != null &&
-            user.uid === post.authorId
-          "
+          v-if="this.$route.name === 'index'"
           align="center"
           justify="end"
           class="mr-0"
         >
-          <v-icon small @click="deletePost(post.postId)"
+          <v-icon
+            v-if="user != null && user.uid === post.authorId"
+            small
+            @click="deletePost(post.postId)"
             >mdi-trash-can-outline</v-icon
           >
+          <v-menu
+            v-if="user != null && user.uid !== post.authorId"
+            bottom
+            offset-y
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on"
+                ><v-icon>mdi-dots-horizontal</v-icon></v-btn
+              >
+            </template>
+            <v-list dense>
+              <v-list-item @click="openDialog">
+                <v-list-item-title>報告</v-list-item-title>
+                <v-dialog v-model="dialog" max-width="500">
+                  <v-card>
+                    <v-card-title class="headline">報告</v-card-title>
+                    <v-card-text>
+                      <form>
+                        <v-textarea
+                          v-model="content"
+                          counter
+                          :error-messages="contentErrors"
+                          label="詳細をお書きください"
+                          required
+                          @blur="$v.content.$touch()"
+                        ></v-textarea>
+                      </form>
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        class="text-capitalize"
+                        depressed
+                        @click="dialog = false"
+                        >Cancel</v-btn
+                      >
+                      <v-btn
+                        color="#ff4081"
+                        class="white--text"
+                        depressed
+                        @click="submit(post.postId)"
+                        >送信</v-btn
+                      >
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-row>
       </v-list-item>
     </v-card-actions>
@@ -34,7 +83,9 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+import { validationMixin } from 'vuelidate'
+import { required, maxLength } from 'vuelidate/lib/validators'
+import firebase from '~/plugins/firebase'
 
 export default {
   filters: {
@@ -42,6 +93,10 @@ export default {
       if (!color) return ''
       return 'is-' + color
     },
+  },
+  mixins: [validationMixin],
+  validations: {
+    content: { required, maxLength: maxLength(500) },
   },
   props: {
     post: {
@@ -54,9 +109,22 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      dialog: false,
+      content: '',
+    }
+  },
   computed: {
     user() {
       return this.$store.getters['auth/user']
+    },
+    contentErrors() {
+      const errors = []
+      if (!this.$v.content.$dirty) return errors
+      !this.$v.content.required && errors.push('必須項目です')
+      !this.$v.content.maxLength && errors.push('500文字以内で入力してください')
+      return errors
     },
   },
   methods: {
@@ -71,6 +139,31 @@ export default {
             this.$emit('set-message', '削除されました。')
           })
       }
+    },
+    openDialog() {
+      setTimeout(() => (this.dialog = true))
+    },
+    submit(postId) {
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        this.sendMail(postId)
+        this.content = null
+        this.dialog = false
+      }
+    },
+    async sendMail(postId) {
+      const functions = firebase.app().functions('asia-northeast1')
+      const sendInvite = functions.httpsCallable('sendMail')
+
+      const res = await sendInvite({
+        dynamic_template_data: {
+          subject: '違反の報告',
+          name: this.user.uid,
+          postId,
+          content: this.content,
+        },
+      })
+      return res
     },
   },
 }
---
+++ functions/src/sendMail.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions')
+const sgMail = require('@sendgrid/mail')
+
+const API_KEY = functions.config().sendgrid.key
+
+sgMail.setApiKey(API_KEY)
+
+exports.sendMail = functions
+  .region('asia-northeast1')
+  .https.onCall(async (data, context) => {
+    const msg = {
+      to: 'hontocha.prog@gmail.com',
+      from: 'hontocha.prog@gmail.com',
+      templateId: 'd-5f041c47f53e4f8bbed195e381ed3fef',
+      dynamic_template_data: {
+        subject: data.subject,
+        name: data.name,
+        postId: data.postId,
+        content: data.content,
+      },
+    }
+    const res = await sgMail.send(msg)
+    return res
+  })
